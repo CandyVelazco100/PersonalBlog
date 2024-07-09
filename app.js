5564ddd873092765be739624a32b1fb5b994292a
Candy Velazco <a20490742@itmexicali.edu.mx>
2024-07-08 19:52:58 -0700

    "Updated app.js to configure Express.js and EJS layouts, added global CSS variables and styles in style.css, and modified index.ejs to update author section and hero image"

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 require('dotenv').config();
 
+// Create an instance of Express.js
 const express = require('express');
+
+// Use Express EJS Layouts for templating
 const expressLayout = require('express-ejs-layouts');
 
+// Create an instance of the Express app
 const app = express();
+
+// Set the port number for the app to listen on 
+// Defaults to 5000, but can be overridden by the PORT environment variable
 const PORT = 5000 || process.env.PORT;
 
+//Serve static files from the 'public' directory
 app.use(express.static('public'));
 
 // Templating Engine
+/**
+ * Configure Express EJS Layouts
+ * Set the layout file to './layouts/main'
+ * Set the view engine to EJS
+ */
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
