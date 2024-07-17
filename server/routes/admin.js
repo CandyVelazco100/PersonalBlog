6a656042419b7071f0228b0d349ce516ad8a17c7
Candy Velazco <a20490742@itmexicali.edu.mx>
2024-07-16 19:40:56 -0700

    "Added admin layout and login functionality to admin route, and created a new header partial for admin views."

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
+const User = require('../models/User');
+
+const adminLayout = '../views/layouts/admin'
 
 router.get('/admin', async (req, res) => {
     try{
@@ -9,7 +12,21 @@ router.get('/admin', async (req, res) => {
             description: 'Simple blog with NodeJS, Express and MongoDB'
         }
 
-        res.render('admin/index', { locals });
+        res.render('admin/index', { locals, layout: adminLayout });
+    }catch(error){
+        console.log(error);
+    }
+});
+
+//Check Login
+router.post('/admin', async (req, res) => {
+    try{
+        const { username, password } = req.body;
+        if(req.body.username === 'admin' && req.body.password === 'password'){
+            res.send('You are logged in');
+        }else{
+            res.send('Wrong username or password');
+        }
     }catch(error){
         console.log(error);
     }
